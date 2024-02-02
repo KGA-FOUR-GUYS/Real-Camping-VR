c7c604eccdc94c83196b735629e0f028eb4aac32
JGoSeungHyeon <144971633+JGoSeungHyeon@users.noreply.github.com>
2024-02-02 10:36:09 +0900

    [delete]Delete Ezyslice
    -Ezyslice 삭제

--- Assets/GSH/3.Script/Slice Cube.cs
+++ Assets/GSH/3.Script/Slice Cube.cs
@@ -4,20 +4,5 @@ using UnityEngine;
 
 public class SliceCube : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.name == "Slice_Obj")
-        {
-            GameObject Piece_Obj_1 = Instantiate(this.gameObject, transform.position, Quaternion.identity);
-            GameObject Piece_Obj_2 = Instantiate(this.gameObject, transform.position, Quaternion.identity);
 
-            Piece_Obj_1.transform.position = new Vector3(Piece_Obj_1.transform.position.x, Piece_Obj_1.transform.position.y, Piece_Obj_1.transform.position.z + 1f);
-            Piece_Obj_2.transform.position = new Vector3(Piece_Obj_2.transform.position.x, Piece_Obj_2.transform.position.y, Piece_Obj_2.transform.position.z - 1f); 
-
-            Piece_Obj_1.transform.localScale = new Vector3(Piece_Obj_1.transform.localScale.x, Piece_Obj_1.transform.localScale.y, Piece_Obj_1.transform.localScale.z * 0.5f);
-            Piece_Obj_2.transform.localScale = new Vector3(Piece_Obj_2.transform.localScale.x, Piece_Obj_2.transform.localScale.y, Piece_Obj_2.transform.localScale.z * 0.5f);
-
-            Destroy(this.gameObject);
-        }
-    }
 }
