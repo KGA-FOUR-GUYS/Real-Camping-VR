19884accf39ad5894fa9a653fb0f720d15772c94
Woo-ChanHyeok <148195213+Woo-ChanHyeok@users.noreply.github.com>
2024-02-05 17:52:06 +0900

    [update] Update Vr_UIMover.cs
    UI이동 구현중
    [in-progress]

--- Assets/WCH/3.Script/Vr_UIMover.cs
+++ Assets/WCH/3.Script/Vr_UIMover.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.UI;
 
 public class Vr_UIMover : MonoBehaviour
 {
+    public ActionBasedController xrController;
     private GameObject hoverUI;
+    private Vector3 lastControllerPosition;
     private bool isHover = false;
+    private bool isFirstGrip = false;
+    private void Awake()
+    {
+        xrController = GetComponentInParent<ActionBasedController>();
+    }
     public void UIHoverEntered(UIHoverEventArgs e)
     {
-        Debug.Log($"{e.uiObject.name} Hover Entered");
         if (e.uiObject.CompareTag("HoverUI"))
         {
             hoverUI = e.uiObject;
@@ -20,21 +27,33 @@ public class Vr_UIMover : MonoBehaviour
 
     public void UIHoverExited(UIHoverEventArgs e)
     {
-        Debug.Log($"{e.uiObject.name} Hover Exited");
         if (e.uiObject.CompareTag("HoverUI"))
         {
             hoverUI = null;
             isHover = false;
+            isFirstGrip = false;
         }
     }
 
     private void Update()
     {
-        if (!isHover)
-            return;
-        else
+        if (!isHover) return;
+
+        if (!isFirstGrip)
         {
-            
+            isFirstGrip = true;
+            lastControllerPosition = xrController.transform.position;
+        }
+
+        if (xrController.selectAction.action.IsPressed())
+        {
+            Debug.Log("is trigger btn");
+            Vector3 currentControllerPosition = xrController.transform.position;
+            Vector3 deltaPosition = currentControllerPosition - lastControllerPosition;
+
+            hoverUI.transform.parent.position += deltaPosition;
+
+            lastControllerPosition = currentControllerPosition;
         }
     }
 }
