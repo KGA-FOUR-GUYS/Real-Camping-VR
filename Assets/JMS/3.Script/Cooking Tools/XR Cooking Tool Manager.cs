c548da8864d02fb2044dc558df880a332802aa50
msjung8634 <msjung8634@gmail.com>
2024-02-05 16:18:09 +0900

    [create] Physical Hand & Cooking Tool 구현 및 연동
    - Physical Hand와 Cooking Tool 구현 완료
      모든 XR Interaction 관련 오브젝트는 Virtual/Physical로 분리하여 구현함.
      Cooking Tool을 잡으면 FixedJoint를 사용해 Physical Tool이 Physical Hand에 붙도록 함.
    [done]
    
    - Fixed Joint 움직임 최적화 필요
      지금은 Fixed Joint끼리 약간 유격이 발생하는 문제가 있지만,
      Physical Tool과 Physical Hand의 MaxSpeed를 높게 설정하여
      임시로 문제 해결한 상태.
    
    - Physical Tool 과 Physical Hand의 거리가 멀어지면 놓치는 기능 추가
      현재 칼만 충돌되도록 빠르게 조작하면, 거리가 멀어지는 것이 가능함.
      이를 활용해, 거리가 일정 수치 이상 멀어지면 놓친 것으로 판정할 수 있음.
    [in-progress]

--- Assets/JMS/3.Script/Cooking Tools/XR Cooking Tool Manager.cs
+++ Assets/JMS/3.Script/Cooking Tools/XR Cooking Tool Manager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRCookingToolManager : MonoBehaviour
 {
     public bool isGrabbed = false;
+    [Range(1f,3f)] public float delayAfterGrabExited = 1f;
 
-    public bool isTrackingPosition = true;
-    public bool isTrackingRotation = true;
+    [Header("Grabbed Option")]
+    public LayerMask grabbedLayer;
+    private LayerMask m_initialLayer;
 
     [Header("Virtual Tool")]
     public GameObject virtualTool;
-    public List<Renderer> virtualToolRenderers = new List<Renderer>();
+    private List<Renderer> m_virtualToolRenderers = new List<Renderer>();
     public bool isVirtualToolVisible = true;
     [Range(.01f, 10f)] public float distanceThreshold = .1f;
 
@@ -19,84 +22,146 @@ public class XRCookingToolManager : MonoBehaviour
 
     [Header("Physical Tool")]
     public GameObject physicalTool;
-    public List<Renderer> physicalToolRenders = new List<Renderer>();
     public float maxSpeed = 5f;
 
     private Transform m_physicalToolTransform;
     private Rigidbody m_physicalToolRigidbody;
 
+    private Rigidbody m_leftHandPhysicalRigidbody;
+    private Rigidbody m_rightHandPhysicalRigidbody;
     private void Awake()
     {
+        m_leftHandPhysicalRigidbody = GameObject.FindGameObjectWithTag("LeftHandPhysical").GetComponent<Rigidbody>();
+        m_rightHandPhysicalRigidbody = GameObject.FindGameObjectWithTag("RightHandPhysical").GetComponent<Rigidbody>();
+
         m_virtualToolTransform = virtualTool.transform;
+        foreach (var renderer in m_virtualToolTransform.GetComponentsInChildren<Renderer>())
+            m_virtualToolRenderers.Add(renderer);
 
         m_physicalToolTransform = physicalTool.transform;
         m_physicalToolRigidbody = physicalTool.GetComponent<Rigidbody>();
+
+        m_initialLayer = m_physicalToolTransform.gameObject.layer;
     }
 
     private void FixedUpdate()
     {
-        UpdatePhysicalToolPosition();
-        UpdatePhysicalToolRotation();
+        // If tool is grabbed
+        // Check distance from interacting physical hand
+        // 손과 거리가 너무 멀어지면 놓치기
     }
 
     private void Update()
     {
+        // If tool is not grabbed
+        // Try to match position and rotation (virtual -> physical)
         UpdateVirtualToolPosition();
         UpdateVirtualToolRotation();
 
         ToggleVirtualHandRenderer();
     }
 
-    private void UpdatePhysicalToolPosition()
-    {
-        if (!isGrabbed || !isTrackingPosition) return;
-
-        // Try to match position (physical -> virtual)
-        var desiredVelocity = (m_virtualToolTransform.position - m_physicalToolTransform.position) / Time.fixedDeltaTime;
-        if (desiredVelocity.magnitude > maxSpeed)
-        {
-            var ratio = maxSpeed / desiredVelocity.magnitude;
-            desiredVelocity = new Vector3(desiredVelocity.x * ratio, desiredVelocity.y * ratio, desiredVelocity.z * ratio);
-        }
-
-        m_physicalToolRigidbody.velocity = desiredVelocity;
-    }
-    private void UpdatePhysicalToolRotation()
-    {
-        if (!isGrabbed || !isTrackingRotation) return;
-
-        // Try to match rotation (physical -> virtual)
-        Quaternion rotationDiff = m_virtualToolTransform.rotation * Quaternion.Inverse(m_physicalToolTransform.rotation);
-        rotationDiff.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
-
-        Vector3 rotationDiffInDegree = angleInDegree * rotationAxis;
-
-        m_physicalToolRigidbody.angularVelocity = (rotationDiffInDegree * Mathf.Deg2Rad) / Time.fixedDeltaTime;
-    }
+    
 
     private void UpdateVirtualToolPosition()
     {
-        if (isGrabbed || !isTrackingPosition) return;
-
-        // Try to match position (virtual -> physical)
+        if (isGrabbed) return;
+        
         m_virtualToolTransform.position = m_physicalToolTransform.position;
     }
     private void UpdateVirtualToolRotation()
     {
-        if (isGrabbed || !isTrackingRotation) return;
+        if (isGrabbed) return;
 
-        // Try to match rotation (virtual -> physical)
         m_virtualToolTransform.rotation = m_physicalToolTransform.rotation;
     }
-
     private void ToggleVirtualHandRenderer()
     {
         float distance = Vector3.Distance(m_virtualToolTransform.position, m_physicalToolTransform.position);
         bool isFar = distance >= distanceThreshold;
 
-        foreach (var renderer in virtualToolRenderers)
+        foreach (var renderer in m_virtualToolRenderers)
         {
             renderer.enabled = isVirtualToolVisible && isFar;
+        }
+    }
+
+    // XR Grab Interactable Events
+    public void OnGrabEntered(SelectEnterEventArgs e)
+    {
+        var interactor = e.interactorObject;
+
+        bool isLeftHand = interactor.transform.gameObject.CompareTag("LeftHandInteractor");
+        bool isRightHand = interactor.transform.gameObject.CompareTag("RightHandInteractor");
+
+        if (!isLeftHand && !isRightHand) return;
+
+        Rigidbody bodyToConnect = null;
+        if (isLeftHand && !isRightHand)
+        {
+            bodyToConnect = m_leftHandPhysicalRigidbody;
+        }
+        else if (!isLeftHand && isRightHand)
+        {
+            bodyToConnect = m_rightHandPhysicalRigidbody;
+        }
+
+        if (bodyToConnect == null) return;
+
+        // 중력을 껐다/켰다 하면 이상해짐... Rigidbody Mass를 조정하는 방식 사용중
+        //m_physicalToolRigidbody.useGravity = false;
+
+        isGrabbed = true;
+        TogglePhysicalToolLayer();
+
+        // Physical Tool의 Position, Rotation 변경
+        Transform attachPoint = interactor.transform.GetChild(0);
+        m_physicalToolTransform.position = attachPoint.position;
+        m_physicalToolTransform.rotation = attachPoint.rotation;
+
+        // FixedJoint 생성, 물리손과 연결
+        var jointToHand = m_physicalToolRigidbody.gameObject.AddComponent<FixedJoint>();
+        jointToHand.connectedBody = bodyToConnect;
+    }
+    public void OnGrabExited(SelectExitEventArgs e)
+    {
+        var interactor = e.interactorObject;
+
+        bool isLeftHand = interactor.transform.gameObject.CompareTag("LeftHandInteractor");
+        bool isRightHand = interactor.transform.gameObject.CompareTag("RightHandInteractor");
+
+        if (!isLeftHand && !isRightHand) return;
+
+        // 중력을 껐다/켰다 하면 이상해짐... Rigidbody Mass를 조정하는 방식 사용중
+        //m_physicalToolRigidbody.useGravity = true;
+
+        isGrabbed = false;
+        Invoke(nameof(TogglePhysicalToolLayer), delayAfterGrabExited);
+
+        // FixedJoint 삭제, 물리손과 분리
+        var jointToHand = m_physicalToolRigidbody.gameObject.GetComponent<FixedJoint>();
+        Destroy(jointToHand);
+    }
+    private void TogglePhysicalToolLayer()
+    {
+        int currentLayerValue = m_physicalToolTransform.gameObject.layer;
+
+        LayerMask targetLayerMask = isGrabbed ? grabbedLayer : m_initialLayer;
+        int targetLayerValue = targetLayerMask == 0
+                                ? 0
+                                : targetLayerMask.value < 0
+                                    ? (int)Mathf.Log(targetLayerMask.value - 1, 2f)
+                                    : (int)Mathf.Log(targetLayerMask.value, 2f);
+
+        if (currentLayerValue == targetLayerValue) return;
+
+        m_physicalToolTransform.gameObject.layer = targetLayerValue;
+
+        // Change layer of children
+        var colliders = m_physicalToolTransform.GetComponentsInChildren<Collider>();
+        foreach (var collider in colliders)
+        {
+            collider.gameObject.layer = targetLayerValue;
         }
     }
 }
