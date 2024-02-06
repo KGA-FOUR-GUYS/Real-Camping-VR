fa774e19738adb13b29553824218077e7f783172
Woo-ChanHyeok <148195213+Woo-ChanHyeok@users.noreply.github.com>
2024-02-06 17:58:56 +0900

    [ui] Update Vr_UIMover.cs
    UI이동 기능 구현 완
    [done]

--- Assets/WCH/3.Script/Vr_UIMover.cs
+++ Assets/WCH/3.Script/Vr_UIMover.cs
@@ -5,55 +5,98 @@ using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.UI;
 
-public class Vr_UIMover : MonoBehaviour
+    
+namespace WCH
 {
-    public ActionBasedController xrController;
-    private GameObject hoverUI;
-    private Vector3 lastControllerPosition;
-    private bool isHover = false;
-    private bool isFirstGrip = false;
-    private void Awake()
+    public class Vr_UIMover : MonoBehaviour
     {
-        xrController = GetComponentInParent<ActionBasedController>();
-    }
-    public void UIHoverEntered(UIHoverEventArgs e)
-    {
-        if (e.uiObject.CompareTag("HoverUI"))
+        public ActionBasedController xrController;
+        public XRRayInteractor xrRayInteractor;
+        private GameObject hoverUI;
+        private Transform hoverUITransform;
+
+        private float hitDistance;
+        private float hitAngle;
+
+        private bool isHover = false;
+        private bool isFirstGrip = false;
+
+        private bool isCoroutine = false;
+
+        private void Awake()
         {
-            hoverUI = e.uiObject;
-            isHover = true;
+            xrController = GetComponentInParent<ActionBasedController>();
+            TryGetComponent(out xrRayInteractor);
         }
-    }
-
-    public void UIHoverExited(UIHoverEventArgs e)
-    {
-        if (e.uiObject.CompareTag("HoverUI"))
+        public void UIHoverEntered(UIHoverEventArgs e)
         {
-            hoverUI = null;
-            isHover = false;
-            isFirstGrip = false;
-        }
-    }
-
-    private void Update()
-    {
-        if (!isHover) return;
-
-        if (!isFirstGrip)
-        {
-            isFirstGrip = true;
-            lastControllerPosition = xrController.transform.position;
+            if (e.uiObject.CompareTag("HoverUI"))
+            {
+                hoverUI = e.uiObject;
+                hoverUITransform = hoverUI.transform.root.GetComponent<Transform>();
+                isHover = true;
+            }
         }
 
-        if (xrController.selectAction.action.IsPressed())
+        public void UIHoverExited(UIHoverEventArgs e)
         {
-            Debug.Log("is trigger btn");
-            Vector3 currentControllerPosition = xrController.transform.position;
-            Vector3 deltaPosition = currentControllerPosition - lastControllerPosition;
+            if (e.uiObject.CompareTag("HoverUI") && !xrController.selectAction.action.IsPressed())
+            {
+                hoverUI = null;
+                isHover = false;
+                isFirstGrip = false;
+            }
+        }
 
-            hoverUI.transform.parent.position += deltaPosition;
+        private void Update()
+        {
+            if (!isHover) return;
 
-            lastControllerPosition = currentControllerPosition;
+
+            if (!isFirstGrip)
+            {
+                isFirstGrip = true;
+                hitDistance = Vector3.Distance(transform.position, hoverUITransform.position);
+                hitAngle = Vector3.Angle(transform.forward, hoverUITransform.forward);
+                Debug.Log(hitDistance);
+            }
+            if (xrController.selectAction.action.IsPressed())
+            {
+                Debug.Log("is trigger btn");
+                Vector3 position = (transform.forward * hitDistance) + transform.position;
+
+                hoverUITransform.position = position;
+                hoverUITransform.rotation = Quaternion.RotateTowards(transform.rotation, hoverUITransform.rotation, hitAngle);
+            }
+            else if (!xrController.selectAction.action.IsPressed())
+            {
+                isFirstGrip = false;
+            }
+        }
+
+        public void SortRotation()
+        {
+            if (isCoroutine) return;
+
+            float rotaionY = hoverUITransform.rotation.eulerAngles.y;
+            //hoverUITransform.rotation = Quaternion.Euler(0, rotaionY, 0);
+            StartCoroutine(SmoothRotate(hoverUITransform, Quaternion.Euler(0, rotaionY, 0), 0.5f));
+        }
+
+        private IEnumerator SmoothRotate(Transform origin, Quaternion target, float time)
+        {
+            isCoroutine = true;
+            float elapsedTime = 0f;
+            while (elapsedTime < time)
+            {
+                Quaternion rotation = Quaternion.Slerp(origin.rotation, target, elapsedTime / time);
+                origin.rotation = rotation;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            origin.rotation = target;
+            isCoroutine = false;
+            yield break;
         }
     }
 }
