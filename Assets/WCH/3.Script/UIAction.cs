af3ee7a37d52662e27253b8875c783e707ca02b2
Woo-ChanHyeok <148195213+Woo-ChanHyeok@users.noreply.github.com>
2024-02-07 17:54:27 +0900

    [create] Create RecipeManager.cs
    레시피 선택시 부드러운 이동 및 확대 구현
    [done]

--- Assets/WCH/3.Script/UIAction.cs
+++ Assets/WCH/3.Script/UIAction.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIAction : MonoBehaviour
 {
-    
+    private TextMeshProUGUI myText;
+    public GameObject instantiateObj;
 
+    private void Awake()
+    {
+        myText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void NextStep()
+    {
+        RecipeManager.instance.uiAction = this;
+        RecipeManager.instance.ProcessChange(CookingProcess.DetailRecipe, myText.text);
+    }
 
 
 
