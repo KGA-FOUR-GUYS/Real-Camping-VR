96110c294ded8126ac5d44e1f0d24727859e32ee
msjung8634 <msjung8634@gmail.com>
2024-02-13 20:35:47 +0900

    [Update] Improve CSVtoSO
    - CSV에서 Image_Name 컬럼 삭제
      Recipe Name / Ingredient Name과 동일한 이름으로 Image 저장하는 것이
      더 효율적이라 판단하여 컬럼 삭제.
    
    - CSV로 IngredientSO 생성하는 기능 추가
      MenuOption에서 GenerateIngredient하면 Overwrite 형태로 IngredientSO 생성됨
      ( Recipe_Ingredient CSV를 GoogleDrive에 저장함 )
    [done]
    
    - Regex를 보완해서 Description에 , (Comma)를 넣을 수 있도록 수정 필요
      현재 Description에 ,(Comma)를 넣으면 파싱이 불가능해지는 문제가 있음.
    
    - Ingredient weightOverVolume을 Script로 지정하는 것 추가 필요
      Animation Curve를 Script로 정해두어야 부피에 대한 가중치를 계산할 수 있으나
      현재 수동으로 해줘야하는 문제가 있음.
    [in-progress]

--- Assets/JMS/3.Script/CSVtoSO.cs
+++ Assets/JMS/3.Script/CSVtoSO.cs
@@ -13,24 +13,23 @@ public class CSVtoSO
     private static string ingredientSOPath = $"Assets/JMS/2.Model/Prefabs/Ingredient";
 
     private static string imagePath = $"Images";
+    private static string materialPath = $"Materials";
+
+    [MenuItem("Cook Data/Generate Recipes", true, 1)]
+    public static bool ValidateGenerateRecipes()
+    {
+        // GenerateRecipes() execute only in edit mode
+        return !EditorApplication.isPlayingOrWillChangePlaymode;
+    }
 
     // Colume Index (0 ~ N)
     private const int RECIPE_NAME = 0;
     private const int RECIPE_DESCRIPTION = 1;
-    private const int RECIPE_IMAGE_NAME = 2;
-    private const int INGREDIENT_NAME = 3;
-    private const int INGREDIENT_QUANTITY = 4;
-    private const int INGREDIENT_COOK_TYPE = 5;
-    private const int INGREDIENT_RIPE_STATE = 6;
-    private const int INGREDIENT_TARGET_VOLUME = 7;
-
-    [MenuItem("Cook Data/Generate Recipes", true, 1)]
-    // GenerateRecipes 실행 전, 유효성 검사
-    public static bool ValidateGenerateRecipes()
-    {
-        // Execute only in edit mode
-        return !EditorApplication.isPlayingOrWillChangePlaymode;
-    }
+    private const int RECIPE_INGREDIENT_NAME = 2;
+    private const int RECIPE_INGREDIENT_QUANTITY = 3;
+    private const int RECIPE_INGREDIENT_COOK_TYPE = 4;
+    private const int RECIPE_INGREDIENT_RIPE_STATE = 5;
+    private const int RECIPE_INGREDIENT_TARGET_VOLUME = 6;
 
     [MenuItem("Cook Data/Generate Recipes", false, 1)]
     public static void GenerateRecipes()
@@ -41,72 +40,124 @@ public class CSVtoSO
         RecipeSO currentRecipe = null;
         RecipeIngredient currentIngredient;
 
+        // Ignore first(header) row
         for (int i = 1; i < lines.Length; i++)
         {
             string[] datas = lines[i].Split(',');
+            bool isEOF = datas[RECIPE_NAME].Equals(string.Empty);
+            bool isRecipeData = !datas[RECIPE_NAME].Equals("NULL");
 
-            // Recipe Info (0 ~ 2)
-            if (!datas[RECIPE_NAME].Equals("NULL"))
+            // Recipe data (0 ~ 1)
+            if (!isEOF && isRecipeData)
             {
-                // Add to asset folder
+                // If new recipe found, create asset with gathered data
                 if (currentRecipe != null)
-                {
                     AssetDatabase.CreateAsset(currentRecipe, $"{recipeSOPath}/{currentRecipe.name}.asset");
-                }
-
-                // End Of File
-                if (datas[RECIPE_NAME].Equals(string.Empty))
-                {
-                    // Save created assets
-                    AssetDatabase.SaveAssets();
-                    AssetDatabase.Refresh();
-
-                    Debug.Log($"Recipe created at '{recipeSOPath}'");
-                    return;
-                }
 
                 // Create empty recipe
                 currentRecipe = ScriptableObject.CreateInstance<RecipeSO>();
-                // Set name
                 currentRecipe.name = datas[RECIPE_NAME];
-                // Set description
                 currentRecipe.description = datas[RECIPE_DESCRIPTION];
-                // Set image
-                currentRecipe.image = Resources.Load<Sprite>($"{imagePath}/{datas[RECIPE_IMAGE_NAME]}");
+                currentRecipe.image = Resources.Load<Sprite>($"{imagePath}/{datas[RECIPE_NAME]}");
             }
-            // Ingredient Info (3 ~ 7)
-            else
+            // Recipe ingredient data (2 ~ 6)
+            else if (!isEOF && !isRecipeData)
             {
                 // Create empty ingredient
                 currentIngredient = new RecipeIngredient();
-                // Set name
-                currentIngredient.name = datas[INGREDIENT_NAME];
-                // Set quantity
-                currentIngredient.quantity = int.Parse(datas[INGREDIENT_QUANTITY]);
-                // Set cookType
-                currentIngredient.cookType = CookingEnumsExtension.ToCookType(datas[INGREDIENT_COOK_TYPE]);
-                // Set ripeState
-                currentIngredient.ripeState = CookingEnumsExtension.ToRipeState(datas[INGREDIENT_RIPE_STATE]);
-                // Set targetVolume
-                currentIngredient.targetVolume = float.Parse(datas[INGREDIENT_TARGET_VOLUME]);
+                currentIngredient.name = datas[RECIPE_INGREDIENT_NAME];
+                currentIngredient.quantity = int.Parse(datas[RECIPE_INGREDIENT_QUANTITY]);
+                currentIngredient.cookType = CookingEnumsExtension.ToCookType(datas[RECIPE_INGREDIENT_COOK_TYPE]);
+                currentIngredient.ripeState = CookingEnumsExtension.ToRipeState(datas[RECIPE_INGREDIENT_RIPE_STATE]);
+                currentIngredient.targetVolume = float.Parse(datas[RECIPE_INGREDIENT_TARGET_VOLUME]);
 
-                // Add ingredient to recipe
+                // Add ingredient to list
                 currentRecipe.ingredientList.Add(currentIngredient);
+            }
+            else if (isEOF)
+            {
+                // If EOF, create asset with gathered data
+                if (currentRecipe != null)
+                    AssetDatabase.CreateAsset(currentRecipe, $"{recipeSOPath}/{currentRecipe.name}.asset");
+
+                // Save created assets
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+
+                Debug.Log($"Recipe created at '{recipeSOPath}'");
             }
         }
     }
 
     [MenuItem("Cook Data/Generate Ingredients", true, 0)]
-    // GenerateIngredients 실행 전, 유효성 검사
     public static bool ValidateGenerateIngredients()
     {
-        // Execute only in edit mode
+        // GenerateIngredients() execute only in edit mode
         return !EditorApplication.isPlayingOrWillChangePlaymode;
     }
+
+    // Colume Index (0 ~ N)
+    private const int INGREDIENT_NAME = 0;
+    private const int INGREDIENT_DESCRIPTION = 1;
+    private const int INGREDIENT_RIPE_FOR_UNDERCOOK = 2;
+    private const int INGREDIENT_RIPE_FOR_WELLDONE = 3;
+    private const int INGREDIENT_RIPE_FOR_OVERCOOK = 4;
+    private const int INGREDIENT_RIPE_FOR_BURN = 5;
+    private const int INGREDIENT_BASE_WEIGHT = 6;
+    // private const int INGREDIENT_VOLUME_WEIGHT = N; // Script로 Animation Curve 그리는 방법 찾기
+    private const int INGREDIENT_MIN_VOLUME = 7;
 
     [MenuItem("Cook Data/Generate Ingredients", false, 0)]
     public static void GenerateIngredients()
     {
-        
+        TextAsset csvData = Resources.Load<TextAsset>(ingredientCSVPath);
+        string[] lines = Regex.Split(csvData.text, "(?=(?:(?:[^\"]*\"){2})*[^\"]*$)\\n");
+
+        IngredientSO currentIngredient = null;
+
+        // Ignore first(header) row
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] datas = lines[i].Split(',');
+            bool isEOF = datas[INGREDIENT_NAME].Equals(string.Empty);
+
+            // Ingredient data (0 ~ 7)
+            if (!isEOF)
+            {
+                // If new ingredient found, create asset with gathered data
+                if (currentIngredient != null)
+                    AssetDatabase.CreateAsset(currentIngredient, $"{ingredientSOPath}/{currentIngredient.name}.asset");
+
+                // Create empty recipe
+                currentIngredient = ScriptableObject.CreateInstance<IngredientSO>();
+                currentIngredient.name = datas[INGREDIENT_NAME];
+                currentIngredient.description = datas[INGREDIENT_DESCRIPTION];
+                currentIngredient.image = Resources.Load<Sprite>($"{imagePath}/{datas[INGREDIENT_NAME]}");
+                currentIngredient.ripeForUndercook = float.Parse(datas[INGREDIENT_RIPE_FOR_UNDERCOOK]);
+                currentIngredient.ripeForWelldone = float.Parse(datas[INGREDIENT_RIPE_FOR_WELLDONE]);
+                currentIngredient.ripeForOvercook = float.Parse(datas[INGREDIENT_RIPE_FOR_OVERCOOK]);
+                currentIngredient.ripeForBurn = float.Parse(datas[INGREDIENT_RIPE_FOR_BURN]);
+                currentIngredient.baseWeight = float.Parse(datas[INGREDIENT_BASE_WEIGHT]);
+                currentIngredient.minVolume = float.Parse(datas[INGREDIENT_MIN_VOLUME]);
+                // Set Materials
+                currentIngredient.rawMaterial = Resources.Load<Material>($"{materialPath}/{datas[INGREDIENT_NAME]}_Raw");
+                currentIngredient.undercookMaterial = Resources.Load<Material>($"{materialPath}/{datas[INGREDIENT_NAME]}_Undercook");
+                currentIngredient.welldoneMaterial = Resources.Load<Material>($"{materialPath}/{datas[INGREDIENT_NAME]}_Welldone");
+                currentIngredient.overcookMaterial = Resources.Load<Material>($"{materialPath}/{datas[INGREDIENT_NAME]}_Overcook");
+                currentIngredient.burnMaterial = Resources.Load<Material>($"{materialPath}/{datas[INGREDIENT_NAME]}_Burn");
+            }
+            else if (isEOF)
+            {
+                // If new ingredient found, create asset with gathered data
+                if (currentIngredient != null)
+                    AssetDatabase.CreateAsset(currentIngredient, $"{ingredientSOPath}/{currentIngredient.name}.asset");
+
+                // Save created assets
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+
+                Debug.Log($"Ingredient created at '{ingredientSOPath}'");
+            }
+        }
     }
 }
