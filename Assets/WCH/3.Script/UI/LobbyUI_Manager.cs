ec7be3f6ab294254e04ca6e5613ac1b94ba0e6c8
Woo-ChanHyeok <148195213+Woo-ChanHyeok@users.noreply.github.com>
2024-02-22 17:54:35 +0900

    [Update] Update LobbyUI_Manager.cs
    로비 UI 디테일작업(버튼색상, 디테일UI, 핀포인트 포커스)
    [done]

--- Assets/WCH/3.Script/UI/LobbyUI_Manager.cs
+++ Assets/WCH/3.Script/UI/LobbyUI_Manager.cs
@@ -11,15 +11,29 @@ public class LobbyUI_Manager : MonoBehaviour
 
     private bool isCoroutine = false;
 
-    [SerializeField] private GameObject Screen_Home;
-    [SerializeField] private GameObject Screen_Map;
-    [SerializeField] private GameObject Screen_Collection;
-    [SerializeField] private GameObject Screen_Setting;
+    [SerializeField] private RectTransform maskRect;
 
-    private RectTransform home_Rect;
-    private RectTransform map_Rect;
-    private RectTransform collection_Rect;
-    private RectTransform setting_Rect;
+    [SerializeField] private RectTransform home_Rect;
+    [SerializeField] private RectTransform map_Rect;
+    [SerializeField] private RectTransform collection_Rect;
+    [SerializeField] private RectTransform setting_Rect;
+
+    private ColorBlock originCB;
+    private ColorBlock SelectedCB;
+
+    private Color normalColor = new Color(100f / 255f, 200f / 255f, 1f);
+    private Color pressedColor = new Color(100f / 255f, 120f / 255f, 1f);
+    [SerializeField] private Button home_Btn;
+    [SerializeField] private Button map_Btn;
+    [SerializeField] private Button collection_Btn;
+    [SerializeField] private Button setting_Btn;
+
+    [SerializeField] private RectTransform map_Img_Rect;
+    [SerializeField] private GameObject map_Detail_GO;
+    [SerializeField] private RectTransform pinPoint_Content;
+    [SerializeField] private RectTransform center;
+    private bool isDetailCo = false;
+    private bool isDetail = false;
 
     private void Awake()
     {
@@ -35,75 +49,59 @@ public class LobbyUI_Manager : MonoBehaviour
 
     private void Start()
     {
-        home_Rect = Screen_Home.GetComponent<RectTransform>();
-        map_Rect = Screen_Map.GetComponent<RectTransform>();
-        //collection_Rect = Screen_Collection.GetComponent<RectTransform>();
-        //setting_Rect = Screen_Setting.GetComponent<RectTransform>();
-
         ChangeActive(home_Rect);
+        originCB = home_Btn.colors;
+
+        SelectedCB = originCB;
+        SelectedCB.normalColor = normalColor;
+        SelectedCB.highlightedColor = normalColor;
+        SelectedCB.selectedColor = normalColor;
+        SelectedCB.pressedColor = pressedColor;
+
     }
 
-    private void ChangeBanner(RectTransform rect)
-    {
-
-    }
-
-    private IEnumerator ScaleChange(RectTransform rect, float time)
+    private IEnumerator MaskSizeChange(RectTransform rect, float time)
     {
         isCoroutine = true;
-
         float elapsedTime = 0f;
-        rect.localScale = Vector3.zero;
-        Vector3 origin = rect.localScale + new Vector3(0, 0.01f, 1);
+
+        float originX = rect.rect.width;
+        float originY = rect.rect.height;
+        
+        Vector2 targetV2 = new Vector2(originX, 10f);
+        Vector2 startV2 = new Vector2(0, 10f);
+
         while (elapsedTime < time)
         {
-            rect.localScale = Vector3.Lerp(origin, new Vector3(1, 0.01f, 1), elapsedTime / time);
+            rect.sizeDelta = Vector2.Lerp(startV2, targetV2, elapsedTime / time);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
+        rect.sizeDelta = targetV2;
+
         elapsedTime = 0f;
-        origin = new Vector3(1, 0.1f, 1);
+        startV2 = targetV2;
+        targetV2 = new Vector2(originX, originY);
+
         while (elapsedTime < time)
         {
-            rect.localScale = Vector3.Lerp(origin, Vector3.one, elapsedTime / time);
+            rect.sizeDelta = Vector2.Lerp(startV2, targetV2, elapsedTime / time);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-
-
-        rect.localScale = Vector3.one;
+        rect.sizeDelta = targetV2;
 
         isCoroutine = false;
+
         yield break;
     }
 
-
-    //private IEnumerator RectMoveSmooth(RectTransform rect,Vector3 targetVector , float time)
-    //{
-    //    float elapsedTime = 0f;
-    //    Vector3 origin = rect.localPosition;
-    //    while (elapsedTime < time)
-    //    {
-    //        rect.position = Vector3.Lerp(origin, targetVector, elapsedTime / time);
-    //        elapsedTime += Time.deltaTime;
-    //        yield return null;
-    //    }
-    //    rect.localPosition = targetVector;
-
-    //    yield break;
-    //}
-
-    public void FocusMapPoint()
-    {
-
-    }
     private void ChangeActive(RectTransform rect)
     {
         home_Rect.gameObject.SetActive(false);
         map_Rect.gameObject.SetActive(false);
-        //todo - screen 만들어지는대로 넣기
-        //home_Rect.gameObject.SetActive(false);
-        //home_Rect.gameObject.SetActive(false);
+        collection_Rect.gameObject.SetActive(false);
+        setting_Rect.gameObject.SetActive(false);
 
         rect.gameObject.SetActive(true);
     }
@@ -115,25 +113,133 @@ public class LobbyUI_Manager : MonoBehaviour
             return;
         }
         ChangeActive(home_Rect);
+        ChangeBtnColor(home_Btn);
         Debug.Log("Home");
-        StartCoroutine(ScaleChange(home_Rect, moveTime));
+        StartCoroutine(MaskSizeChange(maskRect, moveTime));
+        //StartCoroutine(ScaleChange(home_Rect, moveTime));
     }
     public void MapBtn()
     {
+        if (isCoroutine)
+        {
+            return;
+        }
         ChangeActive(map_Rect);
+        ChangeBtnColor(map_Btn);
         Debug.Log("Map");
-        StartCoroutine(ScaleChange(map_Rect, moveTime));
-
+        StartCoroutine(MaskSizeChange(maskRect, moveTime));
+        StartCoroutine(MoveDetail(false, moveTime));
+        map_Img_Rect.localPosition = Vector3.zero;
     }
     public void CollectionBtn()
     {
+        if (isCoroutine)
+        {
+            return;
+        }
+        ChangeActive(collection_Rect);
+        ChangeBtnColor(collection_Btn);
         Debug.Log("Collection");
-
+        StartCoroutine(MaskSizeChange(maskRect, moveTime));
     }
     public void SettingBtn()
     {
+        if (isCoroutine)
+        {
+            return;
+        }
+        ChangeActive(setting_Rect);
+        ChangeBtnColor(setting_Btn);
         Debug.Log("Setting");
+        StartCoroutine(MaskSizeChange(maskRect, moveTime));
+    }
 
+
+    public void FocusMapPoint(RectTransform myRect)
+    {
+        if (isCoroutine)
+        {
+            return;
+        }
+        Vector3 difference = center.position - myRect.position;
+        StartCoroutine(MovetoCenter(map_Img_Rect, difference, moveTime));
+        StartCoroutine(MoveDetail(true, moveTime));
+    }
+    public void CancelFocus()
+    {
+        StartCoroutine(MoveDetail(false, moveTime));
+    }
+
+    private void ChangeBtnColor(Button currentBtn)
+    {
+        
+        home_Btn.colors = originCB;
+        map_Btn.colors = originCB;
+        collection_Btn.colors = originCB;
+        setting_Btn.colors = originCB;
+
+        currentBtn.colors = SelectedCB;
+    }
+
+
+
+    private IEnumerator MovetoCenter(RectTransform origin, Vector3 difference, float time)
+    {
+        isCoroutine = true;
+        float elapsedTime = 0f;
+
+        Vector3 targetV3 = origin.position + difference;
+
+        while (elapsedTime < time)
+        {
+            origin.position = Vector3.Lerp(origin.position, targetV3, elapsedTime / time);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        origin.position = targetV3;
+        isCoroutine = false;
+        yield break;
+    }
+
+    private IEnumerator MoveDetail(bool isOn, float time)
+    {
+        if (isDetailCo)
+        {
+            yield break;
+        }
+        isDetailCo = true;
+        Vector3 origin = map_Detail_GO.transform.localPosition;
+
+        float elapsedTime = 0f;
+        if (isOn && !isDetail)
+        {
+            Vector3 targetV3 = origin + new Vector3(600, 0, 0);
+            isDetail = true;
+            while (elapsedTime < time)
+            {
+                map_Detail_GO.transform.localPosition = Vector3.Lerp(origin, targetV3, elapsedTime / time);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            map_Detail_GO.transform.localPosition = targetV3;
+        }
+
+        else if (!isOn && isDetail)
+        {
+            Vector3 targetV3 = origin - new Vector3(600, 0, 0);
+            isDetail = false;
+            while (elapsedTime < time)
+            {
+                map_Detail_GO.transform.localPosition = Vector3.Lerp(origin, targetV3, elapsedTime / time);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            map_Detail_GO.transform.localPosition = targetV3;
+        }
+
+        isDetailCo = false;
+        yield break;
     }
 
 }
